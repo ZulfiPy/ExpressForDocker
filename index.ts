9f2a793259813fa39de96e20b3df998030d1fc26
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-06-17 09:46:44 +0300

    /api/tasks route configured

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import tasksRouter from "./src/routes/api/tasks";
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const PORT = process.env.PORT
 app.get('/', async (req, res) => {
     res.send('Express + TypeScript server');
 });
+
+app.use('/api/tasks', tasksRouter);
 
 app.listen(PORT, () => {
     console.log(`[server]: Server is running on port ${PORT}`)
